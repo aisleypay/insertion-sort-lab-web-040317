4587d38fd869f3c495e9c3e6fbe9d73f84f8ce7e
aisleypay <paisley.singh@gmail.com>
2017-06-28 09:31:29 -0400

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 function findMinAndRemove(array){
+  let minNum = array[0];
+  let minIndex = 0;
 
+  for(let i=0; i < array.length; i++) {
+    let currEl = array[i]
+    if (currEl < minNum) {
+      minNum = currEl
+      minIndex = i
+    }
+  }
+  array.splice(minIndex,1)
+  return minNum
 }
 
 function insertionSort(array){
+  let sArray = []
 
+  while(array.length !== 0) {
+    let newMin = findMinAndRemove(array)
+    sArray.push(newMin)
+  }
+
+  return sArray;
 }
